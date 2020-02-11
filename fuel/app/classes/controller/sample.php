098304e99a81fde6ba2455892bc854ad8f846e07
uni51 <uni51@users.noreply.github.com>
2020-02-12 06:23:45 +0900

    ビューを使う

--- fuel/app/classes/controller/sample.php
+++ fuel/app/classes/controller/sample.php
@@ -2,8 +2,20 @@
 
 class Controller_Sample extends Controller {
 
+    /**
+     * Chapter2-2：コントローラを使う
+     */
+//    public function action_index() {
+//        print('Hello');
+//        print(date('H:i:s'));
+//    }
+
+    /**
+     * Chapter2-3：ビューを使う
+     */
     public function action_index() {
-        print('Hello');
-        print(date('H:i:s'));
+        $data['hello'] = 'こんにちは';
+
+        return View::forge('sample/index', $data);
     }
 }

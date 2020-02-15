4d0df2dda30bb4e93ddf21eff4b196a8b6f52007
uni51 <uni51@users.noreply.github.com>
2020-02-15 14:34:58 +0900

    URLの最適化

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -18,7 +18,9 @@ return array(
 	 *
 	 */
 
-	'_root_' => 'welcome/index',
+//	'_root_' => 'welcome/index',
+    '_root_' => 'sample/index',
+
 
 	/**
 	 * -------------------------------------------------------------------------

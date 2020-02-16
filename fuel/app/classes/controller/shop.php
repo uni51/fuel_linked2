a784db6d8f8d4c45ebabddc6c0d53c93caa78960
uni51 <uni51@users.noreply.github.com>
2020-02-16 15:51:24 +0900

    Chapter3-3：フォームとの連携

--- fuel/app/classes/controller/shop.php
+++ fuel/app/classes/controller/shop.php
@@ -2,10 +2,34 @@
 
 class Controller_Shop extends Controller {
 
-    public function action_index() {
-        $data = array();
-        $data['rows'] = Model_Item::find_all();
+    /**
+     * Chapter3-2：データベースの内容を表示する
+     */
+//    public function action_index() {
+//        $data = array();
+//        $data['rows'] = Model_Item::find_all();
+//
+//        return View::forge('shop/index', $data);
+//    }
 
-        return View::forge('shop/index', $data);
+
+    /**
+     * Chapter3-3：フォームとの連携
+     */
+    public function action_index() {
+        return View::forge('shop/index');
     }
+
+    public function action_save() {
+        $item = Model_Item::forge();
+
+        $data = array();
+        $data['item_name'] = Input::post('item_name');
+        $data['price'] = Input::post('price');
+        $item->set($data);
+        $item->save();
+
+        print('Saved!!');
+    }
+
 }

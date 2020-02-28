51ece6b23cb9aab6a3011c1e4fc1f4816a172144
uni51 <uni51@users.noreply.github.com>
2020-02-29 08:43:52 +0900

    5-3：フォームの受け取り2

--- fuel/app/classes/controller/contact.php
+++ fuel/app/classes/controller/contact.php
@@ -1,9 +1,21 @@
 <?php
 
-class Controller_Contact extends Controller {
+class Controller_Contact extends Controller_Template {
 
     public function action_index() {
-        return View::forge('contact/index');
+        $this->template->title = 'お問い合わせ';
+        $this->template->content = View::forge('contact/index');
+    }
+
+    public function action_check() {
+        $this->template->title = 'お問い合わせ：確認';
+
+        $data = array();
+        $data['myname'] = Input::post('myname');
+        $data['email'] = Input::post('email');
+        $data['gender'] = Input::post('gender');
+
+        $this->template->content = View::forge('contact/check', $data);
     }
 
 }

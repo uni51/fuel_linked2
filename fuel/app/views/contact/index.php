b3ed212c6b79b7c101f26e244dab3e5a389d57f0
uni51 <uni51@users.noreply.github.com>
2020-02-29 08:18:18 +0900

    5-2：フォームの受け取り1

--- fuel/app/views/contact/index.php
+++ fuel/app/views/contact/index.php
@@ -14,9 +14,29 @@
 
     <div id="content">
         <?php print(Form::open('contact/check')); ?>
-        <?php print(Form::label('お名前', 'myname')); ?>
-        <?php print(Form::input('myname')); ?>
-        <?php print(Form::submit('submit', '送信する')); ?>
+        <table>
+            <tr>
+                <th><?php print(Form::label('お名前', 'myname')); ?></th>
+                <td><?php print(Form::input('myname')); ?></td>
+            </tr>
+            <tr>
+                <th><?php print(Form::label('メールアドレス', 'email')); ?></th>
+                <td><?php print(Form::input('email')); ?></td>
+            </tr>
+            <tr>
+                <th><?php print(Form::label('性別', 'gender')); ?></th>
+                <td>
+                    <?php print(Form::radio('gender', '男性', array('id' => 'form_gender_male'))); ?>
+                    <?php print(Form::label('男性', 'gender_male')); ?>
+                    <?php print(Form::radio('gender', '女性', array('id' => 'form_gender_female', 'checked' => 'checked'))); ?>
+                    <?php print(Form::label('女性', 'gender_female')); ?>
+                </td>
+            </tr>
+            <tr>
+                <th></th>
+                <td><?php print(Form::submit('submit', '送信する')); ?></td>
+            </tr>
+        </table>
         <?php print(Form::close()); ?>
     </div>
 
